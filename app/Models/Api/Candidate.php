c8df686c3650e0a07bacb6c041969aa6e7c28c81
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-01-27 17:25:01 -0300

    Autenticação recrutador

--- app/Models/Api/Candidate.php
+++ app/Models/Api/Candidate.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Candidate extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'technology_id',
+        'name',
+        'email',
+        'age',
+        'url_linkedin'   
+    ];
 }
